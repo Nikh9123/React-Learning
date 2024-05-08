02e98e74da0de41f1564e42c08102da345cedef9
nikh9123 <satyamnikhil1523@gmail.com>
2024-05-08 15:53:37 +0530

    calling the dispatch inside the component event functions

--- react-quiz/src/DateCounter.js
+++ react-quiz/src/DateCounter.js
@@ -80,21 +80,21 @@ function DateCounter() {
           min="0"
           max="10"
           value={step}
-          onChange={defineStep}
+          onChange={(e)=>dispatch({type : "setStep", payload : Number(e.target.value)})}
         />
         <span>{step}</span>
       </div>
 
       <div>
-        <button onClick={dec}>-</button>
-        <input value={count} onChange={defineCount} />
-        <button onClick={inc}>+</button>
+        <button onClick={()=>dispatch({type : "dec"})}>-</button>
+        <input value={count} onChange={(e)=> dispatch({type : "setCount", payload : Number(e.target.value)}) } />
+        <button onClick={()=> dispatch({type :"inc"}) }>+</button>
       </div>
 
       <p>{date.toDateString()}</p>
 
       <div>
-        <button onClick={reset}>Reset</button>
+        <button onClick={()=>dispatch({type :"reset"})}>Reset</button>
       </div>
     </div>
   );
